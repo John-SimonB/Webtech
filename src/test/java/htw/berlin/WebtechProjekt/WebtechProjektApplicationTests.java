e717e187125f739b8ad18284c814509d49a77169
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2021-04-16 12:22:52 +0200

    Add files via upload

---
+++ src/test/java/htw/berlin/WebtechProjekt/WebtechProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.WebtechProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
