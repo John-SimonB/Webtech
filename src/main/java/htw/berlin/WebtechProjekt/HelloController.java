69ca165b7676f9ea7af4b930dfea2bca66dc8398
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2021-04-19 14:19:14 +0200

    Adding the simple "Hello World!" line, with https://spring.io/guides/gs/spring-boot/

---
+++ src/main/java/htw/berlin/WebtechProjekt/HelloController.java
@@ -0,0 +1,14 @@
+package htw.berlin.WebtechProjekt;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+}
--- src/test/java/htw/berlin/WebtechProjekt/WebtechProjektApplicationTests.java
+++ src/test/java/htw/berlin/WebtechProjekt/WebtechProjektApplicationTests.java
@@ -3,11 +3,39 @@ package htw.berlin.WebtechProjekt;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class WebtechProjektApplicationTests {
+import static org.assertj.core.api.Assertions.*;
 
-	@Test
-	void contextLoads() {
+import java.net.URL;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerIT {
+
+	@LocalServerPort
+	private int port;
+
+	private URL base;
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/");
 	}
 
+	@Test
+	public void getHello() throws Exception {
+		ResponseEntity<String> response = template.getForEntity(base.toString(),
+				String.class);
+		assertThat(response.getBody()).isEqualTo("Hello World!");
+	}
 }
