23a63517a00edf72c303a90b79978d14e9e93e3e
Magda <s0574860@htw-berlin.de>
2021-05-19 13:24:27 +0200

    Neuer Pfad für die Registrierung in den Controller hinzugefügt

--- src/main/java/htw/berlin/WebtechProjekt/Controller/ToDoController.java
+++ src/main/java/htw/berlin/WebtechProjekt/Controller/ToDoController.java
@@ -1,15 +1,25 @@
 package htw.berlin.WebtechProjekt.Controller;
 
+import htw.berlin.WebtechProjekt.Registration.RegistrationRequest;
 import htw.berlin.WebtechProjekt.Services.ToDoListService;
+import htw.berlin.WebtechProjekt.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class ToDoController {
 
+    private final UserService userService;
+
+
+    public ToDoController(UserService userService) {
+        this.userService = userService;
+    }
     @Autowired
     public ToDoListService toDoListService;
 
@@ -29,8 +39,20 @@ public class ToDoController {
         return "login";
     }
 
-    @GetMapping(path = "/register")
-    public String showregister(Model model) {
-        return "register";
+    @GetMapping(path = "/registration")
+    public String showRegistrationPage() {
+        return "Registerpage";
+    }
+
+
+    @PostMapping(path = "registration")
+    public String handleRegistrationRequest(RegistrationRequest registrationRequest) {
+        userService.createUser(registrationRequest);
+        return  "?registrationSuccessful";
+    }
+
+    @ModelAttribute(name = "registrationRequest")
+    public RegistrationRequest registrationRequest() {
+        return new RegistrationRequest();
     }
 }
