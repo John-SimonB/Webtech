f7f97c2ae3ed32ed7c36a922b36f49c50956995e
Magda <s0574860@htw-berlin.de>
2021-05-19 13:17:44 +0200

    Die Klassen 'User' und 'Userrolle' für das Einloggen erstellt

---
+++ src/main/java/htw/berlin/WebtechProjekt/Models/ToDoUser.java
@@ -0,0 +1,86 @@
+package htw.berlin.WebtechProjekt.Models;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+public class ToDoUser implements UserDetails {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+
+    public ToDoUser(String firstName,
+                    String lastName,
+                    String email,
+                    String password
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/htw/berlin/WebtechProjekt/Models/UserRole.java
@@ -0,0 +1,5 @@
+package htw.berlin.WebtechProjekt.Models;
+
+public enum UserRole {
+    USER, ADMIN
+}
