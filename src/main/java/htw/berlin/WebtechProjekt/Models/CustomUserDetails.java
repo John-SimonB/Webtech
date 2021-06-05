f47d313a3e1c0ec41e1a2f621278e1a149c99837
Magda <s0574860@htw-berlin.de>
2021-06-05 16:36:02 +0200

    CustomUserDetails-Klasse erstellt (Für das Login notwendig)

---
+++ src/main/java/htw/berlin/WebtechProjekt/Models/CustomUserDetails.java
@@ -0,0 +1,54 @@
+package htw.berlin.WebtechProjekt.Models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private ToDoUser toDoUser;
+    public CustomUserDetails(ToDoUser toDoUser){
+        super();
+        this.toDoUser = toDoUser;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return toDoUser.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return toDoUser.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public String getFullName() {
+        return toDoUser.getFirstName() + " " + toDoUser.getLastName();
+    }
+}
