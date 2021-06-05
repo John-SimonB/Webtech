af077932fbcbd3058a78fd9654a11e8bf5c32d1f
Magda <s0574860@htw-berlin.de>
2021-06-05 16:44:26 +0200

    2 Tests erstellt: "testCreateUser" und "testFindUserByEmail"

---
+++ src/test/java/htw/berlin/WebtechProjekt/UserRepositoryTests.java
@@ -0,0 +1,52 @@
+package htw.berlin.WebtechProjekt;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import htw.berlin.WebtechProjekt.Models.ToDoUser;
+import htw.berlin.WebtechProjekt.Repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateUser(){
+        ToDoUser user = new ToDoUser();
+        user.setFirstName("Magda");
+        user.setLastName("Mnich");
+        user.setEmail("magdamnich@web.de");
+        user.setPassword("password");
+        user.isAccountNonExpired();
+        user.isAccountNonLocked();
+        user.isCredentialsNonExpired();
+        user.isEnabled();
+
+       ToDoUser savedUser = repo.save(user);
+
+      ToDoUser existUser = entityManager.find(ToDoUser.class, savedUser.getId());
+
+      assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+    }
+
+    @Test
+    public void testFindUserByEmail() {
+        String email = "magdamnich@web.de";
+
+        ToDoUser toDoUser = repo.findByEmail(email);
+
+        assertThat(toDoUser).isNotNull();
+    }
+}
