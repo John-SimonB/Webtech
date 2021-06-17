365c784db5529b3a10a8dc5c391d9b1d98bdb240
Magda <s0574860@htw-berlin.de>
2021-06-17 14:34:48 +0200

    Securityordner wurde in Controller verschoben

---
+++ src/main/java/htw/berlin/WebtechProjekt/Controller/Security/PasswordEncoder.java
@@ -0,0 +1,13 @@
+package htw.berlin.WebtechProjekt.Controller.Security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+public class PasswordEncoder {
+
+
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String rawPassword = "";
+        String encodedPassword = encoder.encode((rawPassword));
+        System.out.println(encodedPassword);
+    }
+}
---
+++ src/main/java/htw/berlin/WebtechProjekt/Controller/WebSecurityConfig.java
@@ -0,0 +1,66 @@
+package htw.berlin.WebtechProjekt.Controller;
+
+import htw.berlin.WebtechProjekt.Services.CustomUserDetailsService;
+//import htw.berlin.WebtechProjekt.Services.UserService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // aus dem Tutorial: https://www.youtube.com/watch?v=QwQuro7ekvc
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new CustomUserDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/list_users")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .passwordParameter("password")
+                .usernameParameter("email")
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/login").permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+
+}
