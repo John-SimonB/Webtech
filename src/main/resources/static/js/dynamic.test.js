dada7919306d8e5192d943f9b476598a9d7ba6df
Magda <s0574860@htw-berlin.de>
2021-06-28 08:38:41 +0200

    Frontendtests für empty list message erstellt

---
+++ src/main/resources/static/js/dynamic.test.js
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+import Dynamic from './dynamic.js';
+
+global.axios = {
+    get() { return Promise.resolve({ data: []})},
+    post() { return Promise.resolve('value')}
+}
+
+test('shows empty list message', () => {
+    const wrapper = mount(Dynamic);
+    expect(wrapper.text()).toContain('Es gibt gute Nachrichten! du hast keine Todos');
+})
--- src/main/resources/static/js/dynamicalt.js
+++ src/main/resources/static/js/dynamicalt.js
@@ -1,53 +1,50 @@
 const app = Vue.createApp({})
 app.component('todos', {
     template: `
-     <p></p>
-     <div class="box">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-     <br>
-     <div class="text">Solltest du ein ToDo erledigt haben, dann setz einfach ein Häkchen <img src="image/emoji.png" width="20" height="20" alt="emoji" loading="lazy"></div>
-     <div style="width: 50%; margin: 0px auto;">
-     <p></p>
-     <input v-model="todoField" type="text" placeholder="Aufgabe" class="col-md-4 control-label" ref="todoInput"> <a></a>
-     <input v-model="dateField" type="date" placeholder="Deadline" class="col-md-4 control-label"> <a></a>
-     <button type="button" @click="save()" class="btn btn-success">Speichern</button>
-       <br>
-     </div>
-     </div>
-     <br>
-     <br>
-     <br>
-     <br>
-     <div class="box">
-     <div class="text">Du hast schon {{count}} Todos Erledigt!</div>
-     <p></p>
-     <table class="table">
-         <thead class="thead-dark">
-         <tr>
-             <th></th>
-             <th>Aufgabe</th>
-             <th>Deadline</th>
-             <th></th>
-         </tr>
-         </thead>
-             <tbody>   
-            <tr v-if="items.length === 0">
-                <td></td>
-                <td colspan="2" style="text-align:center">Es gibt gute Nachrichten! du hast keine Todos</td>
-                 <td></td>
-             </tr>
-             <tr v-for="todo in items">
-                <td><input id="oregano-1" type="checkbox" class="checkedBox"/>
-                  <label for="oregano-1" class="checkedLabel">
-                    {{todo.task}}</label></td>
-               <td><label for="oregano-1" class="checkedLabel">
-                 {{todo.deadline}}</label></td>
-                 <td><button class="btntrash" @click="deletetodo(todo.id)"><i class="fa fa-trash"></i></button></td>            
-                 </tr>
-         </tbody>
-         </table>
-         </div>
- `,
+      <p></p>
+      <div class="box">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <br>
+      <div class="text">Solltest du ein ToDo erledigt haben, dann setz einfach ein HÃ¤kchen <img src="image/emoji.png" width="20" height="20" alt="emoji" loading="lazy"></div>
+      <div style="width: 50%; margin: 0px auto;">
+        <p></p>
+        <input v-model="todoField" type="text" placeholder="Aufgabe" class="col-md-4 control-label" ref="todoInput"> <a></a>
+        <input v-model="dateField" type="date" placeholder="Deadline" class="col-md-4 control-label"> <a></a>
+        <button type="button" @click="save()" class="btn btn-success">Speichern</button>
+        <br>
+      </div>
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+      <div class="box">
+      <div class="text">Du hast schon {{count}} Todos Erledigt!</div>
+      <p></p>
+      <table class="table">
+        <thead class="thead-dark">
+        <tr>
+          <th>Aufgabe</th>
+          <th>Deadline</th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-if="items.length === 0">
+          <td></td>
+          <td colspan="2" style="text-align:center">Es gibt gute Nachrichten! du hast keine Todos</td>
+          <td></td>
+        </tr>
+        <tr v-for="todo in items">
+          <td><input id="strikeout" type="checkbox" @click="checkbox()" class="checkedBox"/>
+            <label for="strikeout" class="checkedLabel"> {{todo.task}}</label></td>
+          <td><label for="strikeout" class="checkedLabel">{{todo.deadline}}</label></td>
+          <td><button class="btntrash" @click="deletetodo(todo.id)"><i class="fa fa-trash"></i></button></td>
+        </tr>
+        </tbody>
+      </table>
+      </div>
+    `,
     data() {
         return {
             items: [],
@@ -64,24 +61,25 @@ app.component('todos', {
                     console.log(response.data)
                 })
         },
-        ckeckbox() {
-            var list = document.querySelector('ul');
-            list.addEventListener('click', function (ev) {
-                if (ev.target.tagName === 'LI') {
-                    ev.target.classList.toggle('checked');
-                }
-            }, false);
+        checkbox() {
+            var checkBox = document.getElementsByClassName('checkedBox');
+
+            for( var i = 0; i < checkBox.length; i++){
+                checkBox[i].onclick = function(){
+                    this.parentElement.parentElement.classList.toggle("strikeout");
+                };
+            }
+            console.log('TesTest123');
         },
         deletetodo(idjetzt) {
             axios.delete('/rest/deletetodo/' + idjetzt)
                 .then(response => {
+                    this.loadToDos()
                     this.items.splice(id, 1);
-                    this.loadToDos();
                     console.log(this.items);
                 });
         },
         save() {
-
             axios.post('/todos', {
                 task: this.todoField,
                 deadline: this.dateField,
