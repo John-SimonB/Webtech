5fc0b44cb1f1a31f513cc7752877a466701de673
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2021-06-28 11:28:33 +0200

    Hinzufügen des durchstreichens eines Todos

--- src/main/resources/static/js/dynamicalt.js
+++ src/main/resources/static/js/dynamicalt.js
@@ -1,50 +1,52 @@
 const app = Vue.createApp({})
 app.component('todos', {
     template: `
-      <p></p>
-      <div class="box">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <br>
-      <div class="text">Solltest du ein ToDo erledigt haben, dann setz einfach ein HÃ¤kchen <img src="image/emoji.png" width="20" height="20" alt="emoji" loading="lazy"></div>
-      <div style="width: 50%; margin: 0px auto;">
-        <p></p>
-        <input v-model="todoField" type="text" placeholder="Aufgabe" class="col-md-4 control-label" ref="todoInput"> <a></a>
-        <input v-model="dateField" type="date" placeholder="Deadline" class="col-md-4 control-label"> <a></a>
-        <button type="button" @click="save()" class="btn btn-success">Speichern</button>
-        <br>
-      </div>
-      </div>
-      <br>
-      <br>
-      <br>
-      <br>
-      <div class="box">
-      <div class="text">Du hast schon {{count}} Todos Erledigt!</div>
-      <p></p>
-      <table class="table">
-        <thead class="thead-dark">
-        <tr>
-          <th>Aufgabe</th>
-          <th>Deadline</th>
-          <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-if="items.length === 0">
-          <td></td>
-          <td colspan="2" style="text-align:center">Es gibt gute Nachrichten! du hast keine Todos</td>
-          <td></td>
-        </tr>
-        <tr v-for="todo in items">
-          <td><input id="strikeout" type="checkbox" @click="checkbox()" class="checkedBox"/>
-            <label for="strikeout" class="checkedLabel"> {{todo.task}}</label></td>
-          <td><label for="strikeout" class="checkedLabel">{{todo.deadline}}</label></td>
-          <td><button class="btntrash" @click="deletetodo(todo.id)"><i class="fa fa-trash"></i></button></td>
-        </tr>
-        </tbody>
-      </table>
-      </div>
-    `,
+     <p></p>
+     <div class="box">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+     <br>
+     <div class="text">Solltest du ein ToDo erledigt haben, dann setz einfach ein Häkchen <img src="image/emoji.png" width="20" height="20" alt="emoji" loading="lazy"></div>
+     <div style="width: 50%; margin: 0px auto;">
+     <p></p>
+       <label for="username" class="custom-field three">
+     <input v-model="todoField" type="text" placeholder="Aufgabe" class="custom-file three" ref="todoInput"> <a></a>
+     <input v-model="dateField" type="date" min='2021-06-20' placeholder="Deadline" class="custom-file three"> <a></a>
+     <button type="button" @click="save()" class="btn btn-success">Speichern</button>
+       <br>
+     </div>
+     </div>
+     <br>
+     <br>
+     <br>
+     <br>
+     <div class="box">
+     <div class="text">Du hast schon {{count}} Todos Erledigt!</div>
+     <p></p>
+     <table class="table">
+         <thead class="thead-dark">
+         <tr>
+           <th></th>
+             <th>Aufgabe</th>
+             <th>Deadline</th>
+            <th></th>
+         </tr>
+         </thead>
+             <tbody>   
+            <tr v-if="items.length === 0">
+                <td></td>
+                <td colspan="2" style="text-align:center">Es gibt gute Nachrichten! du hast keine Todos</td>
+                 <td></td>
+             </tr>
+            <tr v-for="todo in items">
+              <td><input id="strikeout" type="checkbox" @click="checkboxclick()" class="checkedBox"/></td>
+              <td id="checkbox" class="">{{todo.task}}</td>
+              <td id="checkbox1" class="">{{todo.deadline}}</td>
+              <td><button class="btntrash" @click="deletetodo(todo.id)"><i class="fa fa-trash"></i></button></td>
+            </tr>
+         </tbody>
+         </table>
+         </div>
+ `,
     data() {
         return {
             items: [],
@@ -54,6 +56,12 @@ app.component('todos', {
         };
     },
     methods: {
+        checkboxclick () {
+            const x = document.getElementById("checkbox");
+            const y = document.getElementById("checkbox1");
+            x.classList.toggle('checkedLabel')
+            y.classList.toggle('checkedLabel')
+        },
         loadToDos() {
             axios.get('/rest/todos')
                 .then(response => {
@@ -62,14 +70,9 @@ app.component('todos', {
                 })
         },
         checkbox() {
-            var checkBox = document.getElementsByClassName('checkedBox');
-
-            for( var i = 0; i < checkBox.length; i++){
-                checkBox[i].onclick = function(){
-                    this.parentElement.parentElement.classList.toggle("strikeout");
-                };
+            if(this.checkbox().checked) {
+                count ++
             }
-            console.log('TesTest123');
         },
         deletetodo(idjetzt) {
             axios.delete('/rest/deletetodo/' + idjetzt)
